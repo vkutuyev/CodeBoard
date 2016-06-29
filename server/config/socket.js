0aa1f8028ba0d71f2dd811631b9bc0b440cc78e3
Howard Jiang <howard.yunghao.jiang@gmail.com>
2016-06-29 09:16:39 -0700

    changed how saving/loading works, can save words now and load words on connection

--- server/config/socket.js
+++ server/config/socket.js
@@ -14,6 +14,7 @@ function Lobby ( id ) {
     this.line_history   = [];
     this.chat_history   = [];
     this.screenshot     = '';
+    this.savestate;
 
     this.id             =  id;
 
@@ -140,9 +141,7 @@ module.exports = function(io) {
                 socket.join(room.id);
             } else {
                 socket.join(room.id);
-                for (var i in room.line_history) {
-                    socket.emit('draw_line', room.line_history[i] );
-                }
+                socket.emit('load_canv', room.savestate)
             }
         })
         socket.on('draw_line', function (data) {
@@ -162,6 +161,10 @@ module.exports = function(io) {
             room.screenshot = data.canvas;
             io.to(room.id).emit('load_canv', data.canvas);
         });
+        socket.on('savestate', function(data) {
+            var room = grabRoom(data.lobby, allLobbies);
+            room.savestate = data.canvas;
+        })
 
         ////////////////////////////////////////////////////////////
         //                    CHATS CONTROLLER                    //
