4c5275fce3b6129f30f9e7d9832676bdc4c049c0
Howard Jiang <howard.yunghao.jiang@gmail.com>
2016-06-29 10:26:39 -0700

    bandaid on GET /null. Menu placed, awaiting menu

--- server/config/routes.js
+++ server/config/routes.js
@@ -4,7 +4,10 @@ var main = require('../controllers/main.js');
 //                        Routes                        //
 //////////////////////////////////////////////////////////
 module.exports = function(app) {
-    app.get('/', function(req, res) {
-        main.index(req, res);
+    // app.get('/', function(req, res) {
+    //     main.index(req, res);
+    // })
+    app.get('/null', function(req, res) {
+        console.log('reached null....error')
     })
 }
